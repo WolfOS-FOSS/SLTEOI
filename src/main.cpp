fdda71b582bece994083a45d49033de3e0480e36
WolfOS-FOSS <goobertheprotgen@gmail.com>
2024-12-05 02:45:38 +0000

    Fix more stuff

--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,62 @@
 #include "core.h"
 
 class LTEApp : public QWidget {
-    Q_OBJECT
+    Q_OBJECT  // Add this line to enable Qt's meta-object system
 
 private:
     LTECore lteCore;
     QLabel *statusLabel;
 
 public:
-    LTEApp(QWidget *parent = nullptr) : QWidget(parent) {
-        QVBoxLayout *layout = new QVBoxLayout(this);
-        Q_OBJECT
-        QPushButton *startButton = new QPushButton("Start LTE Core", this);
-        QPushButton *stopButton = new QPushButton("Stop LTE Core", this);
-        statusLabel = new QLabel("Core Status: Stopped", this);
-
-        layout->addWidget(statusLabel);
-        layout->addWidget(startButton);
-        layout->addWidget(stopButton);
-
-        connect(startButton, &QPushButton::clicked, this, &LTEApp::startCore);
-        connect(stopButton, &QPushButton::clicked, this, &LTEApp::stopCore);
-
-        setLayout(layout);
-        setWindowTitle("LTE-over-IP Simulator");
-    }
+    LTEApp(QWidget *parent = nullptr);  // Constructor
+    ~LTEApp();  // Destructor
 
 private slots:
-    void startCore() {
-        lteCore.start();
-        updateStatus();
-    }
-
-    void stopCore() {
-        lteCore.stop();
-        updateStatus();
-    }
-
-    void updateStatus() {
-        statusLabel->setText(QString::fromStdString(lteCore.getStatus()));
-    }
+    void startCore();
+    void stopCore();
+    void updateStatus();
 };
+
+// Constructor implementation
+LTEApp::LTEApp(QWidget *parent) : QWidget(parent) {
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    QPushButton *startButton = new QPushButton("Start LTE Core", this);
+    QPushButton *stopButton = new QPushButton("Stop LTE Core", this);
+    statusLabel = new QLabel("Core Status: Stopped", this);
+
+    layout->addWidget(statusLabel);
+    layout->addWidget(startButton);
+    layout->addWidget(stopButton);
+
+    connect(startButton, &QPushButton::clicked, this, &LTEApp::startCore);
+    connect(stopButton, &QPushButton::clicked, this, &LTEApp::stopCore);
+
+    setLayout(layout);
+    setWindowTitle("LTE-over-IP Simulator");
+}
+
+// Destructor implementation
+LTEApp::~LTEApp() {
+    stopCore();  // Ensure the core is stopped when the app is closed
+}
+
+// Slot to start the core
+void LTEApp::startCore() {
+    lteCore.start();
+    updateStatus();
+}
+
+// Slot to stop the core
+void LTEApp::stopCore() {
+    lteCore.stop();
+    updateStatus();
+}
+
+// Slot to update the status label
+void LTEApp::updateStatus() {
+    statusLabel->setText(QString::fromStdString(lteCore.getStatus()));
+}
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);

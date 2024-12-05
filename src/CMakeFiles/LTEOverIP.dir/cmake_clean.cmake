70a5760be7d6b4a9bb2be5a826eed6044479bdc7
WolfOS-FOSS <goobertheprotgen@gmail.com>
2024-12-05 02:41:22 +0000

    Commit build

---
+++ src/CMakeFiles/LTEOverIP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LTEOverIP.dir/core.cpp.o"
+  "CMakeFiles/LTEOverIP.dir/main.cpp.o"
+  "LTEOverIP"
+  "LTEOverIP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LTEOverIP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/LTEOverIP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LTEOverIP.
+# This may be replaced when dependencies are built.
